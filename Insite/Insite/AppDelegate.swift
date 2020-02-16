a586b487c60fbb2b8fcc843f8bd16d06a6cd865c
Yasoob Rasheed <yasoobrasheed@Yasoobs-MacBook-Pro.local>
2020-02-16 16:30:40 -0600

    added Firebase

--- Insite/Insite/AppDelegate.swift
+++ Insite/Insite/AppDelegate.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import Firebase
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -26,6 +27,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         self.window?.rootViewController = mapViewController
         self.window?.makeKeyAndVisible()
         
+        FirebaseApp.configure()
         return true
     }
 
